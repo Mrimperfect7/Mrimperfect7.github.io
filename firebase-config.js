8ddb1d7308a0142edbdbe31735b67ddcf37370f4
Mrimperfect7 <110906985+Mrimperfect7@users.noreply.github.com>
2023-09-08 11:20:51 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBvcyjJPrNSkoiI68g8J9mxvH3NX10p2HA",
+  authDomain: "pypro-2ebfb.firebaseapp.com",
+  databaseURL: "https://pypro-2ebfb-default-rtdb.firebaseio.com",
+  projectId: "pypro-2ebfb",
+  storageBucket: "pypro-2ebfb.appspot.com",
+  messagingSenderId: "555786381036",
+  appId: "1:555786381036:web:8d4800e7d972bc01fc67c9",
+  measurementId: "G-JFTSNFGCWP"
+};
